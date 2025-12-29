c71e214c13761e76fa088cf6f9265966dc4c0d9d
Tony Biondo <tonyb@tonybox.net>
2025-12-29 08:40:44 -0500

    tweak ping interval

--- ping.sh
+++ ping.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PINGDATA=$(fping -i 50 -c 5 -i 1 ${TARGET} 2>&1 | tail -n1)
+PINGDATA=$(fping -c 5 -p 100 ${TARGET} 2>&1 | tail -n1)
 
 PING=$(echo $PINGDATA | cut -d'/' -f8)
 LOSS=$(echo $PINGDATA | cut -d'/' -f5 | cut -d'%' -f1)
