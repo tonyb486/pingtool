af074e71d2b1687c8c046ed400f37c3a5ec35c44
Tony Biondo <tonyb@tonybox.net>
2025-12-29 10:07:43 -0500

    noisy

--- ping.sh
+++ ping.sh
@@ -5,4 +5,6 @@ PINGDATA=$(fping -c ${NUM_PINGS} -p 100 -t 5000 -q ${TARGET} 2>&1)
 PING=$(echo $PINGDATA | cut -d'/' -f8)
 LOSS=$(echo $PINGDATA | cut -d'/' -f5 | cut -d'%' -f1)
 
+echo $PINGDATA
+
 rrdtool update /data/ping.rrd -t ping:loss N:$PING:$LOSS
