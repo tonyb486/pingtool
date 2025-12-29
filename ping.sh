0a5ee1f8322a2511046332de4e5a05242f0e1237
Tony Biondo <tonyb@tonybox.net>
2025-12-29 08:36:32 -0500

    quiet

--- ping.sh
+++ ping.sh
@@ -2,11 +2,7 @@
 
 PINGDATA=$(fping -i 50 -c 5 -i 1 ${TARGET} 2>&1 | tail -n1)
 
-echo $PINGDATA
-
 PING=$(echo $PINGDATA | cut -d'/' -f8)
 LOSS=$(echo $PINGDATA | cut -d'/' -f5 | cut -d'%' -f1)
-
-echo $PING $LOSS
 
 rrdtool update /data/ping.rrd -t ping:loss N:$PING:$LOSS
