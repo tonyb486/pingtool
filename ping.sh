628590928650cd2065a3a68f96f2f8c3d8b7d791
Tony Biondo <tonyb@tonybox.net>
2025-12-29 08:24:07 -0500

    fix loss

--- ping.sh
+++ ping.sh
@@ -5,6 +5,6 @@ PINGDATA=$(fping -i 50 -c 5 -i 1 ${TARGET} 2>&1 | tail -n1)
 echo $PINGDATA
 
 PING=$(echo $PINGDATA | cut -d'/' -f8)
-LOSS=$(echo $PINGDATA | cut -d'/' -f8 | cut -d'%' -f1)
+LOSS=$(echo $PINGDATA | cut -d'/' -f5 | cut -d'%' -f1)
 
 rrdtool update /data/ping.rrd -t ping:loss N:$PING:$LOSS
